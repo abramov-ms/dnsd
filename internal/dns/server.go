1003c077e620b4ca811b81ee7f3e595af60dac31
Maxim Abramov <jailthehorny@gmail.com>
2024-04-01 17:13:45 +0300

    Send error responses on failure

--- internal/dns/server.go
+++ internal/dns/server.go
@@ -27,49 +27,60 @@ func NewServer(db Db, host string, port int) (*Server, error) {
 }
 
 func (s Server) Run() {
+loop:
 	for {
-		var buffer [1024]byte
-		_, addr, err := s.conn.ReadFromUDP(buffer[:])
+		var buffer [512]byte
+		bytes, addr, err := s.conn.ReadFromUDP(buffer[:])
 		if err != nil {
 			log.Println("Error reading from UDP socket: ", err)
 			continue
 		}
 
-		log.Printf("Got request from %v\n", *addr)
-		request, _ := ParseMessage(buffer[:])
-
+		log.Println("Got request from ", addr.String())
 		var response Message
 		response.Header = &Header{}
 		response.Header.Response = true
-		response.Header.ID = request.Header.ID
-		response.Header.OpCode = request.Header.OpCode
 		response.Question = make([]Question, 0)
 		response.Authority = make([]Record, 0)
 		response.Answer = make([]Record, 0)
 		response.Additional = make([]Record, 0)
+
+		request, size, err := ParseMessage(buffer[:])
+		if err != nil || size != bytes {
+			log.Println("Bad DNS request detected")
+			response.Header.RCode = FormatError
+			response.Put(buffer[:])
+			s.conn.WriteToUDP(buffer[:], addr)
+			continue
+		}
+
+		response.Header.ID = request.Header.ID
+		response.Header.OpCode = request.Header.OpCode
+		response.Question = request.Question
 
 		for _, q := range request.Question {
 			if q.QType != QType(A) || q.QClass != QClass(IN) {
 				response.Header.RCode = NotImplemented
 				response.Put(buffer[:])
 				s.conn.WriteToUDP(buffer[:], addr)
-			} else {
-				domain := strings.Join(q.Name, ".")
-				record, ok := s.db[domain]
-				if ok {
-					response.Answer = append(response.Answer, *record)
-					response.Header.AnswerRecords++
-				}
+				continue loop
+			}
+
+			name := strings.Join(q.Name, ".")
+			record, ok := s.db[name]
+			if ok {
+				response.Answer = append(response.Answer, *record)
+				response.Header.AnswerRecords++
 			}
 		}
 
-		size := response.Put(buffer[:])
+		size = response.Put(buffer[:])
 		_, err = s.conn.WriteToUDP(buffer[:size], addr)
 		if err != nil {
 			log.Println("Error writing to UDP socket: ", err)
 			continue
 		}
 
-		log.Printf("%v's request served\n", *addr)
+		log.Println("Request served for ", addr.String())
 	}
 }
